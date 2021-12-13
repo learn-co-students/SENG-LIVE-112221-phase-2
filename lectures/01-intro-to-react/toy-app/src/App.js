dea0f5b9a37907f051c7563d7fd15933a947ddc1
Louis Michael Medina <louis@Louis-Medinas-MacBook-Pro.local>
2021-12-13 13:47:19 -0500

    add break out activity instructions

--- lectures/01-intro-to-react/toy-app/src/App.js
+++ lectures/01-intro-to-react/toy-app/src/App.js
@@ -1,6 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+/* 
+    - Break Out Activity #1: Take HTML from Toy Tales lab (Lines 9 - 48) and turn it into JSX
+        - https://github.com/learn-co-curriculum/phase-1-challenge-toy-tale/blob/master/index.html
+        - What needs to change? What's the same?
+        - Refactor HTML into separate components
+
+    - Break Out Activity #2: Implement a ToyCard Component
+        - Start by hard-coding the JSX in your App component return statement
+        - Use props to make your component reusable
+        - Copy/paste data from the "toys.js" file as props and iterate over Toy objects
+          to create a ToyCard component for each
+        - BONUS: Try importing and storing data from the "toys.js" file
+*/
+
 function App() {
   return (
     <div className="App">
