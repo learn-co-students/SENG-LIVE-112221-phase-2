23c2c4020aa7fdadee0791baecd4ebee1f3033cb
Louis Michael Medina <louis@Louis-Medinas-MacBook-Pro.local>
2021-12-13 18:09:32 -0500

    add solutions for 01-intro-to-react

--- lectures/01-intro-to-react/toy-app/src/App.js
+++ lectures/01-intro-to-react/toy-app/src/App.js
@@ -1,6 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
-
 /* 
     - Break Out Activity #1: Take HTML from Toy Tales lab (Lines 9 - 48) and turn it into JSX
         - https://github.com/learn-co-curriculum/phase-1-challenge-toy-tale/blob/master/index.html
@@ -15,24 +12,84 @@ import './App.css';
         - BONUS: Try importing and storing data from the "toys.js" file
 */
 
+// Break Out Activity #2: Import Toys Array from toys.js
+import MyToys from './toys';
+
+// Break Out Activity #2: Import Custom Components
+import ToyCard from './components/ToyCard';
+
+// Root App Component Function
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    return (
+        // Must return JSX as one Parent Level Element
+        <div>
+            {/* Break Out Activity #1: Refactor HTML into JSX */}
+            
+            {/* Header Component */}
+            <div id="toy-header">
+                <img
+                    src="https://fontmeme.com/permalink/180719/67429e6afec53d21d64643101c43f029.png"
+                    alt="toy-header"
+                />
+            </div>
+
+            {/* Container Component */}
+            <div className="container">
+                
+                {/* Form Component */}
+                <form className="add-toy-form">
+                    <h3>Create a toy!</h3>
+
+                    {/* Input Component */}
+                    <input
+                        type="text"
+                        name="name"
+                        // value=""
+                        placeholder="Enter a toy's name..."
+                        className="input-text"
+                    />
+                    <br />
+
+                    {/* Input Component */}
+                    <input
+                        type="text"
+                        name="image"
+                        // value=""
+                        placeholder="Enter a toy's image URL..."
+                        className="input-text"
+                    />
+                    <br />
+
+                    {/* Input Component */}
+                    <input
+                        type="submit"
+                        name="submit"
+                        value="Create Toy"
+                        className="submit"
+                    />
+                </form>
+            </div>
+            
+            {/* Hero Component */}
+            
+            <p style={{textAlign: 'center'}}>
+                Andy needs your help! <button id="new-toy-btn">Add a new toy!</button>
+            </p>
+
+            {/* ToyCollection Component */}
+            <div id="toy-collection">
+        
+                {/* Map over Array of Toy Objects */}
+                {MyToys.map(toy => {
+                
+                    // For each Toy object, create a ToyCard Component and pass
+                    // the Toy object as a prop, "toy"
+                    return (
+                        <ToyCard key={toy.id} toy={toy}/>
+                    );
+                })}
+            </div>
+        </div>
   );
 }
 
